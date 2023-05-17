844166d44ea5817810abda12b8907103a52c2d10
Jonathan Hernandez <pgr.gni.20@gmail.com>
2023-05-17 14:47:51 -0600

    Estado, render y su actualizacion

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,15 @@
+import React from 'react'
+
 const Search = () => {
+
+    const [ todoValue, setTodoValue ] = React.useState('');
+    console.log(todoValue);
+
     return(
         <input placeholder="Nuevo TODO" 
+        value = { todoValue }
         onChange={ (event) => {
-            console.log(event.target.value);
+            setTodoValue(event.target.value);
         } }
         type="text"/>
     );
